5bc1ef2b22b40597618202ae7ee3b45106f1d815
HugoDoesStuff <40184498+HugoDoesStuff@users.noreply.github.com>
2018-06-13 13:51:50 -0700

    Add files via upload

---
+++ MyJavaScript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+   document.getElementById("demo").innerHTML = "Haha Just Kidding.";
+}
+
+window.alert("I don't know who you are. I don't know what you want. If you are looking for a ransom, I can tell you, I don't have money. But what I do have are a very particular set of skills. Skills I've acquired over a very long career. Skills that make me a nightmare for people like you. If you let my daughter go now, that'll be the end of it. I will not look for you. I will not pursue you. But if you don't, I will look for you. I will find you......and I will kill you.")
+
+var x = 5;
+var y = 10;
+document.yesManyes("dem").innerHTML = x + y;
